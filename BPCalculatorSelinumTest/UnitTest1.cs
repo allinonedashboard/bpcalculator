94e557f617246593c3d82b91e429534068fc423e
Trinath <trinath.chakka@synchronoss.com>
2022-12-11 20:12:50 +0000

    add Selenium Tests

---
+++ BPCalculatorSelinumTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace BPCalculatorSelinumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        private String webAppUri;
+
+        [TestInitialize]                // run before each unit test
+        public void Setup()
+        {
+            // read URL from SeleniumTest.runsettings
+            //this.webAppUri = testContextInstance.Properties["webAppUri"].ToString();
+            this.webAppUri = "https://bpcheck.azurewebsites.net";
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
+            if (chromeDriverPath is null)
+            {
+                chromeDriverPath = "/usr/local/bin";                 // for IDE
+            }
+            using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
+            {
+
+                driver.Navigate().GoToUrl(webAppUri);
+                IWebElement bpSystolic = driver.FindElement(By.Id("Bp_Systolic"));
+                bpSystolic.SendKeys("70");
+
+                IWebElement bpDiastolic = driver.FindElement(By.Id("BP_Diastolic"));
+                bpDiastolic.SendKeys("40");
+
+                driver.FindElement(By.Id("form1")).Submit();
+
+                // explictly wait for result with "BPValue" item
+                IWebElement BPValueElement = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+                    .Until(c => c.FindElement(By.Id("bpVal")));
+
+                // item comes back like "BMIValue: 24.96"
+                String bp = BPValueElement.Text.ToString();
+
+            }
+        }
+    }
+}
